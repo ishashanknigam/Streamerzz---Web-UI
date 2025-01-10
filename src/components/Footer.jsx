9de2c9f689507e2b67e49591e215efc805c26a27
Shashank <shashankn@student.iul.ac.in>
2025-01-10 19:13:15 +0530

    Add files via upload

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import { FOOTER_CONTENT } from "../constants"
 
 const Footer = () => {
   return (
-    <footer className=" mt-20 text-neutral-400">
+    <footer className=" mt-22 text-neutral-400">
       <div className="max-w-7xl mx-auto px-4 border-t border-neutral-800">
 
         <div className="grid grid-cols-1 md:grid-cols-4 gap-8 mt-20">
@@ -20,7 +20,7 @@ const Footer = () => {
           ))}
         </div>
 
-        <div className="my-12 border-t border-neutral-800 pt-8 text-center text-sm text-neutral-500">
+        <div className="my-8  border-t border-neutral-800 pt-8 text-center text-sm text-neutral-500">
 
           <div className="flex justify-between">
             <div className="text-xs">
